0c66412f1184064a03099dac85feb65508ccdb2a
사과삼 <70147289+appler3@users.noreply.github.com>
2021-08-27 21:00:07 +0900

    login-signup js update

--- project_test/src/main/webapp/resources/js/login-signup.js
+++ project_test/src/main/webapp/resources/js/login-signup.js
@@ -5,23 +5,21 @@ $(document).ready(function(){
         if($(this).val().length > 10){           
             $('.id-label').css({
                 "display" : "block",
+                "margin-left" : "5px",
+                "margin-top" : "5px",
                 "color" : "red"
             });
             $('.id-label').html('10글자 내로 작성하세요.');
             $(this).select();
         } else if($(this).val().length <= 0){
-            if($(this).val().length <= 0){           
-                $('.id-label').css({
-                    "display" : "block",
-                    "color" : "red"
-                });
-                $('.id-label').html('1~10글자 내로 작성하세요.');
-                $(this).select();
-            } else {
-                $('.id-label').css({
-                    "display" : "none",
-                });
-            }
+            $('.id-label').css({
+                "display" : "block",
+                "margin-left" : "5px",
+                "margin-top" : "5px",
+                "color" : "red"
+            });
+            $('.id-label').html('1~10글자 내로 작성하세요.');
+            $(this).select();            
         } else {
             $('.id-label').css({
                 "display" : "none",
@@ -34,23 +32,21 @@ $(document).ready(function(){
         if($(this).val().length > 20){           
             $('.pwd-label').css({
                 "display" : "block",
+                "margin-left" : "5px",
+                "margin-top" : "5px",
                 "color" : "red"
             });
             $('.pwd-label').html('20글자 내로 작성하세요.');
             $(this).select();
         } else if($(this).val().length <= 0){
-            if($(this).val().length <= 0){           
-                $('.pwd-label').css({
-                    "display" : "block",
-                    "color" : "red"
-                });
-                $('.pwd-label').html('1~20글자 내로 작성하세요.');
-                $(this).select();
-            } else {
-                $('.pwd-label').css({
-                    "display" : "none",
-                });
-            }
+            $('.pwd-label').css({
+                "display" : "block",
+                "margin-left" : "5px",
+                "margin-top" : "5px",
+                "color" : "red"
+            });
+            $('.pwd-label').html('1~20글자 내로 작성하세요.');
+            $(this).select();            
         } else {
             $('.pwd-label').css({
                 "display" : "none",
