422f1b5c7e86e9893cba4ab24f4c03a2e305fe2e
사과삼 <70147289+appler3@users.noreply.github.com>
2021-08-27 19:55:50 +0900

    Add files via upload

---
+++ project_test/src/main/webapp/resources/js/login-signup.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+    /* signup - 회원가입 */
+    // id 체크
+    $('#id').change(function(){ 
+        if($(this).val().length > 10){           
+            $('.id-label').css({
+                "display" : "block",
+                "color" : "red"
+            });
+            $('.id-label').html('10글자 내로 작성하세요.');
+            $(this).select();
+        } else if($(this).val().length <= 0){
+            if($(this).val().length <= 0){           
+                $('.id-label').css({
+                    "display" : "block",
+                    "color" : "red"
+                });
+                $('.id-label').html('1~10글자 내로 작성하세요.');
+                $(this).select();
+            } else {
+                $('.id-label').css({
+                    "display" : "none",
+                });
+            }
+        } else {
+            $('.id-label').css({
+                "display" : "none",
+            });
+        }
+    });
+    
+    // pwd 체크
+    $('#pwd').change(function(){ 
+        if($(this).val().length > 20){           
+            $('.pwd-label').css({
+                "display" : "block",
+                "color" : "red"
+            });
+            $('.pwd-label').html('20글자 내로 작성하세요.');
+            $(this).select();
+        } else if($(this).val().length <= 0){
+            if($(this).val().length <= 0){           
+                $('.pwd-label').css({
+                    "display" : "block",
+                    "color" : "red"
+                });
+                $('.pwd-label').html('1~20글자 내로 작성하세요.');
+                $(this).select();
+            } else {
+                $('.pwd-label').css({
+                    "display" : "none",
+                });
+            }
+        } else {
+            $('.pwd-label').css({
+                "display" : "none",
+            });
+        }
+    });
+
+
+})
